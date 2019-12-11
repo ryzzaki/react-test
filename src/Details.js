444b147b16c3511be323d5b932aedcbeb18490e3
ryzzaki <cuong.nugi@gmail.com>
2019-12-11 23:51:06 +0000

    adding a carousel

--- src/Details.js
+++ src/Details.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import pet from '@frontendmasters/pet';
+import Carousel from './Carousel';
 
 class Details extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      loading: true,
-    };
-  }
+  state = { loading: true }; // use this instead of the constructor thanks to @babel/preset-react
 
   componentDidMount() {
     pet.animal(this.props.id).then(({ animal }) => {
@@ -28,10 +23,11 @@ class Details extends React.Component {
       return <h1>Loading...</h1>;
     }
 
-    const { animal, name, location, description, breed } = this.state;
+    const { animal, name, location, description, breed, media } = this.state;
 
     return (
       <div className="details">
+        <Carousel media={media} />
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} - ${breed} - ${location}`}</h2>
